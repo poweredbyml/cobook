fd7aa55c0466978f255e7918caee5c790184ced9
Aravind Chowdary <kamaravichow@gmail.com>
2020-09-12 08:38:40 +0530

    initial setup

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import {
+  DefaultTheme,
+  Provider as PaperProvider,
+  Appbar,
+} from "react-native-paper";
 
-export default function App() {
+import NotesScreen from "./components/Notes";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "tomato",
+    accent: "yellow",
+  },
+};
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Appbar style={styles.bottom}>
+      <Appbar.Action
+        icon="archive"
+        onPress={() => console.log("Pressed archive")}
+      />
+      <Appbar.Action icon="mail" onPress={() => console.log("Pressed mail")} />
+      <Appbar.Action
+        icon="label"
+        onPress={() => console.log("Pressed label")}
+      />
+      <Appbar.Action
+        icon="delete"
+        onPress={() => console.log("Pressed delete")}
+      />
+    </Appbar>
+  );
+}
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  bottom: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "#fff",
   },
 });
